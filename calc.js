369abe54a3b06bea32f8c21899b18fcd8c243adf
gonthan <cbhi121291@gn.iwasaki.ac.jp>
2022-06-30 14:37:45 +0900

    ドットの二重打ち止めとルートの計算の完成

--- calc.js
+++ calc.js
@@ -1,16 +1,33 @@
+let Dot = 0;
 function Calc(btn) {
-    if(btn.value == "=") {
-      document.calc.display.value = eval(document.calc.display.value);
-    } else if (btn.value == "C") {
+  let Btn = btn.value;  
+  let Result = document.calc.display.value;
+    if(Btn == "=") {
+      document.calc.display.value = eval(Result);
+      Dot = 0;
+    } else if (Btn == "AC") {
       document.calc.display.value = "";
+      Dot = 0;
     } else {
-      if (btn.value == "x") {
-        btn.value = "*";
-      } else if (btn.value == "÷") {
-        btn.value = "/";
-      } 
-      document.calc.display.value += btn.value;
+      if (Btn == "x") {
+        Btn = "*";
+        Dot = 0;
+      } else if (Btn == "÷") {
+        Btn = "/";
+        Dot = 0;
+      }else if (Btn == "."){
+        if(Dot == 1){
+        Btn = ""; 
+        } else{
+          Dot = 1; 
+        }
+      }
+      document.calc.display.value += Btn;
       document.calc.add_btn.value = "x";
       document.calc.div_btn.value = "÷";
     }
+  }
+
+  function Route(){
+    document.calc.display.value = Math.sqrt(parseInt(document.calc.display.value));
   }
